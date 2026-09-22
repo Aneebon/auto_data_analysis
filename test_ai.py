2c6450c9317a6924de196d3562f47bdb78e640bb
Anudip Saha <156197136+Aneebon@users.noreply.github.com>
2026-09-22 19:33:25 +0530

    Update test_ai.py

--- test_ai.py
+++ test_ai.py
@@ -1,32 +1,9 @@
 import pandas as pd
 
 from auto_data_analysis.ai.chart_analyzer import analyze_dataset
-
-
-# ============================================================
-# LOAD CLEANED DATASET
-# ============================================================
-
 df = pd.read_csv(
     "output/cleaned_data.csv"
 )
-
-
-# ============================================================
-# ASK AI
-# ============================================================
-
 print("\nSending dataset information to Hugging Face...")
-
 response = analyze_dataset(df)
-
-
-# ============================================================
-# DISPLAY RESPONSE
-# ============================================================
-
-print("\n==============================")
-print("AI CHART PLAN")
-print("==============================")
-
 print(response)
